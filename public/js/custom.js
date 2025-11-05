76fb1c627ec54fdd872bb40055e0efd9edd5c0ff
chh92-eng <chh92@cornell.edu>
2025-11-05 11:26:50 -0500

    Fix and clean up custom.css project spacing

--- public/js/custom.js
+++
@@ -1,9 +0,0 @@
-document.addEventListener("DOMContentLoaded", function() {
-  const worksBtn = document.querySelector(".works-button");
-  if (worksBtn) {
-    worksBtn.addEventListener("click", () => {
-      window.location.href = worksBtn.getAttribute("href");
-    });
-  }
-});
-
